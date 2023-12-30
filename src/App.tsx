d20727de21b32403beb6cc466886a4196533894e
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-30 07:37:24 +0530

    pages & components setup & Route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
+import Loader from './components/Loader';
+
+
 const App = () => {
     return (
-        <div>App Compppp</div>
+        <>
+            <Router>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/cart" element={<Cart />} />
+                    </Routes>
+                </Suspense>
+            </Router>
+        </>
     )
 }
 
